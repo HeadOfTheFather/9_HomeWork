f48dd6c061ab8681d0c26e1e0f1cfc3a8a3c1e70
Alexandr <headofthefather@gmail.com>
2023-01-15 00:43:09 +0300

    9_HomeWork is done

---
+++ 9_HomeWork/Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.WriteLine("Enter the first number: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int Sum = 0;
+
+void SumOfNaturalElemets(int M, int N)
+{
+    if (M >= 0 && N >= 0)
+    {
+        if (M < N)
+        {
+            Sum += M + N;
+            SumOfNaturalElemets(M+1,N-1);
+        }
+        else if (M > N)
+        {
+            Sum += M + N;
+            SumOfNaturalElemets(M-1,N+1);
+        }  
+        else if (M == N)
+        {   
+            Sum += (M + N)/2;
+            Console.WriteLine($"Sum of all natural elements between your numbers is: {Sum}");
+        }  
+    } 
+    else Console.WriteLine("ERROR: One of your number is negative!");
+ }
+SumOfNaturalElemets(M, N);
